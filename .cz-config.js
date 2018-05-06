8294b9bd23b3fc31d64049a5f99a9ae06f35e68f
Dan Hedgecock <hedgecock.d@gmail.com>
2018-05-06 07:45:25 -0700

    chore: magical commit prompts 🔮

--- .cz-config.js
+++ .cz-config.js
@@ -2,35 +2,28 @@
 
 module.exports = {
   types: [
-    { value: 'feat', name: 'feat:     A new feature' },
-    { value: 'fix', name: 'fix:      A bug fix' },
-    { value: 'docs', name: 'docs:     Documentation only changes' },
-    {
-      value: 'style',
-      name:
-        'style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-    },
+    { value: 'feat', name: '🎉 feat:       A new feature' },
+    { value: 'fix', name: '👍 fix:        A bug fix' },
     {
       value: 'refactor',
       name:
-        'refactor: A code change that neither fixes a bug nor adds a feature',
+        '🔩 refactor:   A code change that neither fixes a bug nor adds a feature',
     },
     {
-      value: 'perf',
-      name: 'perf:     A code change that improves performance',
+      value: 'test',
+      name: '✅ test:       Updating tests or adding missing tests',
     },
-    { value: 'test', name: 'test:     Adding missing tests' },
     {
       value: 'chore',
       name:
-        'chore:    Changes to the build process or auxiliary tools and libraries such as documentation generation',
+        '⚙️  chore:      Changes to the build process, development tools, project configs, etc.',
     },
-    { value: 'revert', name: 'revert:   Revert to a commit' },
-    { value: 'WIP', name: 'WIP:      Work in progress' },
+    { value: 'docs', name: '📝 docs:       Documentation only changes' },
+    { value: 'WIP', name: '🚧 wip:        Work in progress' },
+    { value: 'revert', name: '↩️  revert:     Revert to a commit' },
   ],
-
-  scopes: [{ name: 'loader' }, { name: 'plugin' }, { name: 'example' }],
 
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix', 'style', 'refactor', 'perf', 'chore'],
+  scopes: [{ name: 'loader' }, { name: 'plugin' }, { name: 'example' }],
 }
