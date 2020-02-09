fb7807957a05693d715afbc5abb410ef8d8379dc
Dan Hedgecock <hedgecock.d@gmail.com>
2020-02-08 20:36:12 -0800

    Chore: Update package dependencies 🆙

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,14 @@
 module.exports = {
   root: true,
   extends: 'eloquence/node',
+
+  overrides: [
+    {
+      files: ['*.spec.js'],
+      parserOptions: {
+        // Using Babel parser allows writing tests as ESModules now
+        sourceType: 'script',
+      },
+    },
+  ],
 }
