5f21a198824e660226fef3f88b63aecd876ffefb
Dan Hedgecock <hedgecock.d@gmail.com>
2020-05-24 17:41:24 -0700

    Chore: Update Eloquence 🆙

--- test-app/.eslintrc.js
+++ test-app/.eslintrc.js
@@ -1,16 +1,5 @@
 'use strict'
 
-// Use react linting with Node resolver b/c the eslint-webpack resolver isn't
-// installed
-module.exports = {
-  root: true,
-  extends: 'eloquence/react',
+const eloquence = require('eslint-config-eloquence')
 
-  settings: {
-    'import/resolver': 'node',
-  },
-
-  rules: {
-    'import/no-unresolved': 'off',
-  },
-}
+module.exports = eloquence({ target: 'react' })
