cd6c02bdf08d0451e972b694acb4c6a6c486ccf6
Dan Hedgecock <Hedgecock.D@gmail.com>
2020-09-23 18:39:02 -0700

    Update: webpack-sources (#112)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,11 @@
 
 const eloquence = require('eslint-config-eloquence')
 
-const configs = eloquence({ target: 'node', esm: false })
+const configs = eloquence({ target: 'node', enableESM: false, enableTS: false })
 configs.overrides.push({
   files: ['src/browser/**/*'],
   parserOptions: {
     sourceType: 'module',
-  },
-  rules: {
-    'node/no-unsupported-features/es-syntax': 'off',
   },
 })
 
